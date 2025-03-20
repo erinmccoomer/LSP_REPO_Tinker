52df4f28c6b24c20f7dbc60f1c401a9fcddc164c
erinmccoomer <168048280+erinmccoomer@users.noreply.github.com>
2025-03-20 16:45:57 -0400

    Add files via upload

---
+++ LSP_Spring_2025/src/org/howard/edu/lsp/midterm/question1/HelloWorldSample.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.midterm.question1;
+
+public class HelloWorldSample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World Sample Test");
+
+	}
+
+}
